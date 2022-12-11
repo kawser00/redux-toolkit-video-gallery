e9e36d287bca5025d9255a0d326960293df119e2
kawser00 <kawsarmahmud358@gmail.com>
2022-12-11 13:09:45 +0600

    [UPDATE] api hosting

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://lws-redux-api-server.herokuapp.com',
+  baseURL: 'https://json-server-apis.onrender.com',
 });
 
 export default axiosInstance;
